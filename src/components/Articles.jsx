7007255e7c6a9fbaafbd6682d404603f0567cbc0
Mchumbles <lewis.mcguire92@yahoo.com>
2024-11-05 10:25:43 +0000

    Enable SingleArticle component, with some styling

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "../../api";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import SingleArticle from "./SingleArticle";
 
 export default function Articles() {
   const [currArticles, setCurrArticles] = useState([]);
@@ -14,16 +16,18 @@ export default function Articles() {
   return (
     <section>
       <ul className="article-list">
-        {currArticles.map((article) => {
-          return (
-            <Card className="article-card" key={article.article_id}>
-              <Card.Header>{`Article Title: ${article.title}`}</Card.Header>
-              <Card.Text>{`Topic: ${article.topic}`}</Card.Text>
-              <Card.Text>{`Author: ${article.author}`}</Card.Text>
-              <Card.Text>{`Date Posted: ${article.created_at}`}</Card.Text>
-            </Card>
-          );
-        })}
+        {currArticles.map((article) => (
+          <Card className="article-card" key={article.article_id}>
+            <Card.Header>
+              <Link to={`/articles/${article.article_id}`}>
+                <Card.Title>{article.title}</Card.Title>
+              </Link>
+            </Card.Header>
+            <Card.Text>{`Topic: ${article.topic}`}</Card.Text>
+            <Card.Text>{`Author: ${article.author}`}</Card.Text>
+            <Card.Text>{`Date Posted: ${article.created_at}`}</Card.Text>
+          </Card>
+        ))}
       </ul>
     </section>
   );
