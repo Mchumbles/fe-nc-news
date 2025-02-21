73722013edbccd7fbfde613aa282ac28eeb7f3fa
Mchumbles <lewis.mcguire92@yahoo.com>
2025-02-21 10:50:15 +0000

    Implement mobile responsiveness to header and footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,26 @@
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  const navOptions =
-    "pl-10 pr-10 pb-2 pt-2 text-white text-xl border-l border-r border-white hover:shadow-xl";
-
   return (
-    <nav
-      className="flex items-center justify-center bg-blue-800 pb-6"
-      aria-label="Main Navigation"
-    >
-      <ul className="flex">
+    <nav className="hidden md:flex items-center justify-center bg-blue-800 pb-4">
+      <ul className="flex space-x-6">
         <li>
-          <Link
-            className={navOptions}
-            to="/"
-            role="link"
-            aria-label="Go to Home page"
-          >
+          <Link className="text-white text-lg hover:underline" to="/">
             Home
           </Link>
         </li>
         <li>
-          <Link
-            className={navOptions}
-            to="/articles"
-            role="link"
-            aria-label="Go to Articles page"
-          >
+          <Link className="text-white text-lg hover:underline" to="/articles">
             Articles
           </Link>
         </li>
         <li>
-          <Link
-            className={navOptions}
-            to="/topics"
-            role="link"
-            aria-label="Go to Topics page"
-          >
+          <Link className="text-white text-lg hover:underline" to="/topics">
             Topics
           </Link>
         </li>
         <li>
-          <Link
-            className={navOptions}
-            to="/users"
-            role="link"
-            aria-label="Go to Users page"
-          >
+          <Link className="text-white text-lg hover:underline" to="/users">
             Users
           </Link>
         </li>
