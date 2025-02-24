e4140b749dc5808f2f71cd278bd6daf2cab92d7c
Mchumbles <lewis.mcguire92@yahoo.com>
2025-02-24 09:56:39 +0000

    Refactor ArticlesByTopic and ArticleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function ArticleCard({ article }) {
   return (
-    <li className="pb-6 w-full flex justify-center">
+    <li className="pb-6 w-full flex justify-center" key={article.article_id}>
       <article
-        className="relative bg-white rounded-lg shadow-md p-6 flex flex-col md:flex-row items-start justify-between w-full max-w-[36rem]"
+        className="relative bg-white rounded-lg shadow-md p-6 flex flex-col justify-center items-center w-full max-w-[36rem] min-h-[18rem] md:flex-row md:items-center md:justify-between"
         aria-labelledby={`article-${article.article_id}`}
       >
-        <div className="flex-1 pr-0 md:pr-4">
-          <div className="border-b border-blue-800 pb-4 mb-4">
+        <div className="flex-1 flex flex-col justify-center items-center text-center md:items-start md:text-left">
+          <div className="border-b border-blue-800 pb-4 mb-4 w-full">
             <Link
               className="hover:underline no-underline"
               to={`/articles/${article.article_id}`}
@@ -39,12 +39,12 @@ export default function ArticleCard({ article }) {
         <Link
           to={`/articles/${article.article_id}`}
           aria-label={`View article image: ${article.title}`}
-          className="mt-4 md:mt-0"
+          className="mt-4 md:mt-0 md:self-center md:absolute md:bottom-4 md:right-4"
         >
           <img
             alt={`Image related to article titled ${article.title}`}
-            className="w-24 h-24 md:w-36 md:h-36 object-cover rounded-lg"
             src={article.article_img_url}
+            className="w-28 h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 object-cover rounded-lg mx-auto md:mx-0"
           />
         </Link>
       </article>
--- src/components/ArticlesByTopic.jsx
+++ src/components/ArticlesByTopic.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "./Loading";
 import { fetchArticleByTopic } from "../../api";
-import { Link } from "react-router-dom";
 import ArticleQueries from "./ArticleQueries";
 import Error from "./Error";
+import ArticleCard from "./ArticleCard";
 
 export default function ArticlesByTopic() {
   const { slug } = useParams();
@@ -57,56 +57,7 @@ export default function ArticlesByTopic() {
 
       <ul role="list" className="flex flex-col items-center">
         {currArticles.map((article) => (
-          <li
-            className="pb-6 w-full flex justify-center"
-            key={article.article_id}
-          >
-            <article
-              className="relative bg-white rounded-lg shadow-md p-6 flex flex-col md:flex-row items-start justify-between w-full max-w-[36rem]"
-              aria-labelledby={`article-${article.article_id}`}
-            >
-              <div className="flex-1 pr-0 md:pr-4">
-                <div className="border-b border-blue-800 pb-4 mb-4">
-                  <Link
-                    className="hover:underline no-underline"
-                    to={`/articles/${article.article_id}`}
-                    aria-label={`Read full article: ${article.title}`}
-                  >
-                    <h3
-                      id={`article-${article.article_id}`}
-                      className="text-2xl font-semibold text-gray-800"
-                    >
-                      {article.title}
-                    </h3>
-                  </Link>
-                </div>
-
-                <Link
-                  to={`/topics/${article.topic}`}
-                  className="text-blue-500 hover:underline no-underline"
-                  aria-label={`View more articles about ${article.topic}`}
-                >
-                  <p className="text-sm text-black">{`Topic: ${article.topic}`}</p>
-                </Link>
-
-                <p className="text-sm text-black">{`Author: ${article.author}`}</p>
-                <p className="text-sm text-black">{`Votes: ${article.votes}`}</p>
-                <p className="text-sm text-black">{`Date Posted: ${article.formattedDate}`}</p>
-              </div>
-
-              <Link
-                to={`/articles/${article.article_id}`}
-                aria-label={`View article image: ${article.title}`}
-                className="mt-4 md:mt-0"
-              >
-                <img
-                  alt={`Image related to article titled ${article.title}`}
-                  className="w-24 h-24 md:w-36 md:h-36 object-cover rounded-lg"
-                  src={article.article_img_url}
-                />
-              </Link>
-            </article>
-          </li>
+          <ArticleCard key={article.article_id} article={article} />
         ))}
       </ul>
     </section>
